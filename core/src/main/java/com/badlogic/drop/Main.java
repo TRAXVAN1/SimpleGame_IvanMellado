9b1f8f558799f9caa506bb9758d33893ecf43aeb
TRAXVAN1 <a23ivamelher@inspedralbes.cat>
2025-04-29 19:18:06 +0200

    IvanSimpleGame Acabado

---
+++ core/src/main/java/com/badlogic/drop/Main.java
@@ -0,0 +1,211 @@
+package com.badlogic.drop;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main implements ApplicationListener {
+    Texture backgroundTexture;
+    Texture bucketTexture;
+    Texture dropTexture;
+    Sound dropSound;
+    Music music;
+
+    SpriteBatch spriteBatch;
+    FitViewport viewport;
+
+    Sprite bucketSprite;
+
+    Vector2 touchPos;
+
+    Array<Sprite> dropSprites;
+
+    float dropTimer;
+
+    Rectangle bucketRectangle;
+    Rectangle dropRectangle;
+
+    private int score;
+    private BitmapFont font;
+    private SpriteBatch batch;
+
+
+    @Override
+    public void create() {
+        backgroundTexture = new Texture("background.png");
+        bucketTexture = new Texture("bucket.png");
+        dropTexture = new Texture("drop.png");
+        dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.mp3"));
+        music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+
+        spriteBatch = new SpriteBatch();
+        viewport = new FitViewport(8, 5);
+
+        bucketSprite = new Sprite(bucketTexture);
+        bucketSprite.setSize(1,1);
+
+        touchPos = new Vector2();
+
+        dropSprites = new Array<>();
+
+        bucketRectangle = new Rectangle();
+        dropRectangle = new Rectangle();
+
+        music.setLooping(true);
+        music.setVolume(0.5f);
+        music.play();
+
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        font.getData().setScale(2); //escala del texto
+        score = 0; // puntuaje
+
+
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height,true);
+
+    }
+
+    @Override
+    public void render() {
+        input();
+        logic();
+        draw();
+    }
+
+    private void input(){
+        float speed = 4f;
+        float delta = Gdx.graphics.getDeltaTime(); // retrieve the current delta
+
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            bucketSprite.translateX(speed * delta); // Move the bucket right
+        }else if (Gdx.input.isKeyPressed(Keys.LEFT)) {
+            bucketSprite.translateX(-speed * delta); // Move the bucket right
+        }
+
+        if(Gdx.input.isTouched()){
+            touchPos.set(Gdx.input.getX(),Gdx.input.getY());
+            viewport.unproject(touchPos);
+            bucketSprite.setCenterX(touchPos.x);
+        }
+    }
+    private void logic(){
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+
+        float bucketWidth = bucketSprite.getWidth();
+        float bucketHeight = bucketSprite.getHeight();
+
+        bucketSprite.setX(MathUtils.clamp(bucketSprite.getX(),0,worldWidth - bucketWidth));
+
+        float delta = Gdx.graphics.getDeltaTime();
+
+        bucketRectangle.set(bucketSprite.getX(),bucketSprite.getY(),bucketWidth,bucketHeight);
+
+
+        for (int i = dropSprites.size - 1; i >= 0; i--) {
+            Sprite dropSprite = dropSprites.get(i);
+            float dropWidth = dropSprite.getWidth();
+            float dropHeight = dropSprite.getHeight();
+
+            dropSprite.translateY(-2f * delta);
+            dropRectangle.set(dropSprite.getX(),dropSprite.getY(),dropWidth,dropHeight);
+
+            if (dropSprite.getY() < -dropHeight) dropSprites.removeIndex(i);
+            else if (bucketRectangle.overlaps(dropRectangle)) {
+                    dropSprites.removeIndex(i);
+                    dropSound.play();
+                    score ++; // augmenta el contador del score
+            }
+        }
+
+        dropTimer += delta;
+        if(dropTimer >= 1){
+            dropTimer = 0;
+            createDoplet();
+        }
+
+
+    }
+    private void draw(){
+        ScreenUtils.clear(Color.BLACK);
+        viewport.apply();
+        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
+        spriteBatch.begin();
+
+        // emmagatzema el worldWidth i worldHeight com a variables locals
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+
+
+        spriteBatch.draw(backgroundTexture, 0, 0, worldWidth, worldHeight); //dibuixa el fons
+        bucketSprite.draw(spriteBatch);
+
+        for(Sprite dropSprite : dropSprites){
+            dropSprite.draw(spriteBatch);
+        }
+        spriteBatch.end();
+        // dibujar texto en coordenadas de pantalla
+        spriteBatch.setProjectionMatrix(spriteBatch.getProjectionMatrix().idt().setToOrtho2D(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
+        spriteBatch.begin();
+        font.getData().setScale(2); // escala normal para píxeles
+        font.draw(spriteBatch, "Puntuació: " + score, 20, Gdx.graphics.getHeight() - 20);
+        spriteBatch.end();
+
+    }
+
+    private void createDoplet(){
+        float dropWidth = 1;
+        float dropHeight = 1;
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+
+        Sprite dropSprite = new Sprite(dropTexture);
+        dropSprite.setSize(dropWidth,dropHeight);
+        dropSprite.setX(MathUtils.random(0f,worldWidth - dropWidth));
+        dropSprite.setY(worldHeight);
+        dropSprites.add(dropSprite);
+    }
+
+    @Override
+    public void pause() {
+        // Invoked when your application is paused.
+    }
+
+    @Override
+    public void resume() {
+        // Invoked when your application is resumed after pause.
+    }
+
+    @Override
+    public void dispose() {
+
+        //en el tutorial no ho fa pero com hem apres a lliberar recursos cuan tanquem la app en el adoid he buscar con fer-ho en java
+        backgroundTexture.dispose();
+        bucketTexture.dispose();
+        dropTexture.dispose();
+        dropSound.dispose();
+        music.dispose();
+        spriteBatch.dispose();
+        font.dispose();    }
+}
